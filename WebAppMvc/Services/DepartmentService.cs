b9fabc3a47c2e9b059ba71bf5f6ca7d7b2eef3a5
Leandro Salgado <leandro.salgado@reditpro.com>
2022-02-09 15:13:32 +0000

    Async and waits through Tasks

--- WebAppMvc/Services/DepartmentService.cs
+++ WebAppMvc/Services/DepartmentService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebAppMvc.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebAppMvc.Services
 {
@@ -15,9 +16,9 @@ namespace WebAppMvc.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
