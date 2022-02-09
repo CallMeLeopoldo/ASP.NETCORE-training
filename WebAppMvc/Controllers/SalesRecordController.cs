0e1b2f66d89c78e44ed5b7a496f0d7a2f8f21132
Leandro Salgado <leandro.salgado@reditpro.com>
2022-02-09 16:24:30 +0000

    Added Simple Search

---
+++ WebAppMvc/Controllers/SalesRecordController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using WebAppMvc.Services;
+using System.Threading.Tasks;
+
+namespace WebAppMvc.Controllers
+{
+    public class SalesRecordController : Controller
+    {
+
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch(DateTime? min, DateTime? max)
+        {
+            if (!min.HasValue)
+            {
+                min = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+
+            if (!max.HasValue)
+            {
+                max = DateTime.Now;
+            }
+            ViewData["minDate"] = min.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = max.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateAsync(min, max);
+            return View(result);
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAppMvc/Services/SalesRecordService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppMvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAppMvc.Services
+{
+    public class SalesRecordService
+    {
+        private readonly WebAppMvcContext _context;
+
+        public SalesRecordService(WebAppMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? min, DateTime? max)
+        {
+            var result = _context.SalesRecord.Select(x => x);
+
+            if (min.HasValue)
+            {
+                result = result.Where(x => x.Date > min.Value);
+            }
+
+            if (max.HasValue)
+            {
+                result = result.Where(x => x.Date < max.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
